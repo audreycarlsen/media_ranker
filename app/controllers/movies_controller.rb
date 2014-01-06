3efc06f2d22bb5db68175e08d1196722e01cc590
Audrey Carlsen <audreycarlsen@gmail.com>
2014-01-06 15:36:14 -0800

    Created new movies form

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,23 @@
 class MoviesController < ApplicationController
 
   def index
+    @movies = Movie.all
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    @movie.save
+    redirect_to movies_path
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :director, :synopsis, :rank)
   end
 
 end
---
+++ db/migrate/20140106231125_add_default_rank_to_movie.rb
@@ -0,0 +1,5 @@
+class AddDefaultRankToMovie < ActiveRecord::Migration
+  def change
+    change_column :movies, :rank, :integer, default: 0
+  end
+end
